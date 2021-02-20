2708cc878dc05b1d91b7c419832f5e331621dfbb
kiryl-kashuba <kashuba.kirya@gmail.com>
2021-02-21 00:41:02 +0300

    feat: fill entity client

--- src/main/java/com/kashuba/onlinestore/entity/Client.java
+++ src/main/java/com/kashuba/onlinestore/entity/Client.java
@@ -1,4 +1,44 @@
 package com.kashuba.onlinestore.entity;
 
+import java.util.Objects;
+
 public class Client {
+
+    public enum Status {
+        ACTIVE,
+        BLOCKED;
+
+        //getClientStatus
+    }
+
+    private String firstName;
+    private String secondName;
+    private long phoneNumber;
+    private Status status;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return phoneNumber == client.phoneNumber &&
+                Objects.equals(firstName, client.firstName) &&
+                Objects.equals(secondName, client.secondName) &&
+                status == client.status;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, secondName, phoneNumber, status);
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "firstName='" + firstName + '\'' +
+                ", secondName='" + secondName + '\'' +
+                ", phoneNumber=" + phoneNumber +
+                ", status=" + status +
+                '}';
+    }
 }
