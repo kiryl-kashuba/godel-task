ffc1e2891324fd3d359be6ec1ef6726e0af3d0c1
kiryl-kashuba <kashuba.kirya@gmail.com>
2021-02-20 13:32:31 +0300

    feat: fill entity user

--- src/main/java/com/kashuba/onlinestore/entity/User.java
+++ src/main/java/com/kashuba/onlinestore/entity/User.java
@@ -1,4 +1,67 @@
 package com.kashuba.onlinestore.entity;
 
+import java.util.Objects;
+
 public class User {
+
+    public enum Role{
+        GUEST,
+        CLIENT,
+        ADMIN;
+
+        //getUserRole
+    }
+
+    private long id;
+    private String email;
+    private Role role;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", role=" + role +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id &&
+                Objects.equals(email, user.email) &&
+                role == user.role;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, email, role);
+    }
 }
