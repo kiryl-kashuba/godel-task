324422b3ccc9e9ba4e9a67ddab0ec14ebed5cad0
kiryl-kashuba <kashuba.kirya@gmail.com>
2021-02-21 01:14:00 +0300

    feat: fill ServiceProjectException

--- src/main/java/com/kashuba/onlinestore/exeption/ServiceProjectException.java
+++ src/main/java/com/kashuba/onlinestore/exeption/ServiceProjectException.java
@@ -1,4 +1,21 @@
 package com.kashuba.onlinestore.exeption;
 
 public class ServiceProjectException extends Exception {
+
+    public ServiceProjectException() {
+        super();
+    }
+
+    public ServiceProjectException(String message) {
+        super(message);
+    }
+
+    public ServiceProjectException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ServiceProjectException(Throwable cause) {
+        super(cause);
+    }
+
 }
