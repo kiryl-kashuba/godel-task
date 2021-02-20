c3aa71404e681a8b0ffa8496d00e6679069b6c29
kiryl-kashuba <kashuba.kirya@gmail.com>
2021-02-21 01:06:48 +0300

    feat: fill entity productAttribute

--- src/main/java/com/kashuba/onlinestore/entity/ProductAttribute.java
+++ src/main/java/com/kashuba/onlinestore/entity/ProductAttribute.java
@@ -1,4 +1,58 @@
 package com.kashuba.onlinestore.entity;
 
+import java.util.Objects;
+
 public class ProductAttribute {
+
+    private long id;
+    private String name;
+    private boolean mandatory;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isMandatory() {
+        return mandatory;
+    }
+
+    public void setMandatory(boolean mandatory) {
+        this.mandatory = mandatory;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProductAttribute that = (ProductAttribute) o;
+        return id == that.id &&
+                mandatory == that.mandatory &&
+                Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, mandatory);
+    }
+
+    @Override
+    public String toString() {
+        return "ProductAttribute{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", mandatory=" + mandatory +
+                '}';
+    }
 }
