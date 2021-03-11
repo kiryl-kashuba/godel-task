accc8517861271502587d7c7fa8c03169004ddf9
kiryl-kashuba <kashuba.kirya@gmail.com>
2021-03-11 16:40:54 +0300

    feat: i don't know how to create Generic methods

--- percistance/src/main/java/com/kashuba/onlinestore/dao/impl/FileInitializationHolder.java
+++ percistance/src/main/java/com/kashuba/onlinestore/dao/impl/FileInitializationHolder.java
@@ -1,8 +1,17 @@
 package com.kashuba.onlinestore.dao.impl;
 
 import com.kashuba.onlinestore.dao.fileservice.FileInitialization;
+import com.kashuba.onlinestore.entity.BaseEntity;
+
+import java.util.List;
 
 public abstract class FileInitializationHolder {
 
     public FileInitialization fileInitialization = FileInitialization.getInstance();
+
+    public abstract List<?> create(BaseEntity baseEntity);
+
+    public abstract List<?> delete(int id);
+
+    public abstract List<?> find();
 }
