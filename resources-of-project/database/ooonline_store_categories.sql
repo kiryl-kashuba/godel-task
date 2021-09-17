921daeadcb267a87849cba2a93ce88e655df5634
k.k <77916879+kiryl-kashuba@users.noreply.github.com>
2021-09-18 01:21:09 +0300

    Delete resources-of-project/database directory

--- resources-of-project/database/ooonline_store_categories.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO ooonline_store.categories (id, name)
-VALUES (3, 'bots');
-INSERT INTO ooonline_store.categories (id, name)
-VALUES (1, 'laptops');
--- resources-of-project/database/ooonline_store_orders.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO ooonline_store.orders (id, amount, date_order, cart_id, client_id)
-VALUES (1, 3000, '2021-04-29', 1, 1);
