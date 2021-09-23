967f7a3f684e6090240f26a3c065ac70a72cc7b0
Kiryl Kashuba <88317022+kiryl-kashuba-idf@users.noreply.github.com>
2021-09-23 14:37:05 +0300

    feat(DB): add CREATE script to DataLoader

--- src/main/java/com/kashuba/simplewebapp/dataLoader/DataLoader.java
+++ src/main/java/com/kashuba/simplewebapp/dataLoader/DataLoader.java
@@ -8,6 +8,7 @@ import java.sql.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,7 +17,14 @@ public class DataLoader implements ApplicationRunner {
   @Autowired
   private EmployeeService employeeService;
 
+  @Autowired
+  private JdbcTemplate jdbcTemplate;
+
   public void run(ApplicationArguments args) {
+    jdbcTemplate.execute("drop table IF EXISTS employee");
+    jdbcTemplate.execute("create table employee(" +
+        "employee_id serial, first_name varchar(100), last_name varchar(100), department_id integer, job_title "
+        + "varchar(100), gender varchar(30), date_of_birth date)");
     employeeService.create(new EmployeeDto( "Evgen",
         "Borisov", 42L, "back-end developer",
         Gender.MALE, Date.valueOf("2000-12-12")));
